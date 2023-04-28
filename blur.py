7932ba456d5c550dd94a57db69f2b3352a90f2fd
tumble-weed <aniketsinghresearch@gmail.com>
2023-04-28 17:16:32 +0530

    Update blur.py

--- blur.py
+++ blur.py
@@ -1,7 +1,8 @@
-
+import torch
 import torch.nn.functional as F
 
 def gaussian_blur(input_tensor, kernel_size, sigma=None):
+    device =input_tensor.device
     if sigma is None:
         sigma = (kernel_size - 1) / 6  # Impute ideal sigma from kernel size
 
@@ -9,14 +10,15 @@ def gaussian_blur(input_tensor, kernel_size, sigma=None):
 
     # Create Gaussian kernel
     kernel = torch.tensor([
-        [((x - kernel_size // 2) / sigma)**2 for x in range(kernel_size)]
+        [(x - kernel_size // 2)**2 + (y - kernel_size // 2)**2 for x in range(kernel_size)]
         for y in range(kernel_size)
-    ])
+    ], dtype=torch.float,device=device)
+    kernel = torch.exp(-kernel / (2 * sigma**2))
     kernel = kernel / kernel.sum()
     kernel = kernel.view(1, 1, kernel_size, kernel_size)
     kernel = kernel.repeat(channels, 1, 1, 1)
 
     # Apply Gaussian blur
-    blurred = F.conv2d(input_tensor, kernel, stride=1, padding=kernel_size // 2)
+    blurred = F.conv2d(input_tensor, kernel, stride=1, padding=kernel_size // 2, groups=channels)
 
     return blurred
